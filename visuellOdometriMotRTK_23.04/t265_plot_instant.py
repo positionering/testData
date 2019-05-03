7d09768d306a5a62aeadd71302bb1a2dd5c8be4c
Gabriel Skoghäll <skoghall@student.chalmers.se>
2019-05-03 14:33:16 +0200

    fix

--- visuellOdometriMotRTK_23.04/t265_plot_instant.py
+++ visuellOdometriMotRTK_23.04/t265_plot_instant.py
@@ -47,64 +47,64 @@ v_T265_z = []
 with open(filename_T265, 'r') as f:
     for line in f:
     # print(line.split(' ')[0])
-    x = line.split(' ')[3]
-    z = line.split(' ')[5]
+        x = line.split(' ')[3]
+        z = line.split(' ')[5]
 
-    if first:
+        if first:
 
-        first = False
-        t_first = float(line.split(' ')[1]) 
-
-
-    t.append(float(line.split(' ')[1]) - t_first)
-
-    #höger hjulhastighet
-    #v_x = float(line.split()[22].strip(","))
-    #v_y = float(line.split()[23].strip(","))
-    #v_z = float(line.split()[24].strip(","))
-
-    #v_rx.append(v_x)
-    #v_ry.append(v_z)
-    #v_r.append(math.sqrt(v_x**2+v_y**2+v_z**2))
+            first = False
+            t_first = float(line.split(' ')[1]) 
 
 
-    #vänster hjulhastighet
+        t.append(float(line.split(' ')[1]) - t_first)
 
-    v_x = float(line.split()[11].strip(","))
-    v_y = float(line.split()[12].strip(","))
-    v_z = float(line.split()[13].strip(","))
+        #höger hjulhastighet
+        #v_x = float(line.split()[22].strip(","))
+        #v_y = float(line.split()[23].strip(","))
+        #v_z = float(line.split()[24].strip(","))
 
-    v_lx.append(v_x)
-    v_ly.append(v_z)
-    v_l.append(math.sqrt(v_x**2+v_y**2+v_z**2))
+        #v_rx.append(v_x)
+        #v_ry.append(v_z)
+        #v_r.append(math.sqrt(v_x**2+v_y**2+v_z**2))
 
-    #T265 speed
 
-    v_T265_x.append(float(line.split()[16].strip(",")))
-    v_T265_z.append(float(line.split()[18]))
+        #vänster hjulhastighet
+
+        v_x = float(line.split()[11].strip(","))
+        v_y = float(line.split()[12].strip(","))
+        v_z = float(line.split()[13].strip(","))
+
+        v_lx.append(v_x)
+        v_ly.append(v_z)
+        v_l.append(math.sqrt(v_x**2+v_y**2+v_z**2))
+
+        #T265 speed
+
+        v_T265_x.append(float(line.split()[16].strip(",")))
+        v_T265_z.append(float(line.split()[18]))
+            
+
+        #pulser
+        tic_r.append(float(line.split()[20]))
+        tic_l.append(float(line.split()[19]))
+
+        x_T265.append(-float(x))
+        z_T265.append(float(z))
+
+
+        offset_x = x_gnss[0]
+        offset_z = z_gnss[0]
+        x_gnss = np.array(x_gnss)
+        z_gnss = np.array(z_gnss)
+
+        x_gnss = x_gnss - offset_x
+        z_gnss = z_gnss - offset_z
+
+        camx, camy = wo_location(tic_l, tic_r, 'Hjuldata from test: ' + t265_file)
+
+        x_T265 = np.array(x_T265)
+        z_T265 = np.array(z_T265)
         
-
-    #pulser
-    tic_r.append(float(line.split()[20]))
-    tic_l.append(float(line.split()[19]))
-
-    x_T265.append(-float(x))
-    z_T265.append(float(z))
-
-
-    offset_x = x_gnss[0]
-    offset_z = z_gnss[0]
-    x_gnss = np.array(x_gnss)
-    z_gnss = np.array(z_gnss)
-
-    x_gnss = x_gnss - offset_x
-    z_gnss = z_gnss - offset_z
-
-    camx, camy = wo_location(tic_l, tic_r, 'Hjuldata from test: ' + t265_file)
-
-    x_T265 = np.array(x_T265)
-    z_T265 = np.array(z_T265)
-    
     # Plottar t265 path
     fig = plt.figure()
     plt.plot(x_T265, z_T265, label='Route from T265')
