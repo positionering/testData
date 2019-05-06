46d4f82d6a35ff2f99727baa373edb6a013a7489
nadim-h <nadimh5@hotmail.com>
2019-05-06 14:23:00 +0200

    grej

--- visuellOdometriMotRTK_23.04/T265/realsense-pose.cpp
+++ visuellOdometriMotRTK_23.04/T265/realsense-pose.cpp
@@ -1,41 +1,23 @@
-// License: Apache 2.0. See LICENSE file in root directory.
-// Copyright(c) 2019 Intel Corporation. All Rights Reserved.
 #include <librealsense2/rs.hpp>
 #include <iostream>
 #include <iomanip>
 #include <fstream>
-
 #include <chrono>
-#include <ctime>
 
-#include <thread>
 #include <mutex>
 #include <cstring>
 #include <cmath>
 
 #include <unistd.h>
+#include <cfloat>
+#include <thread>
 
 #include "cArduino.h"
 #include "motion.h"
 
-#include <cfloat>
+#include <Eigen/Core>
+#include <Eigen/Geometry>
 
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <termios.h>
-
-#include <cstring>
-#include <mutex>
-
-
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <strings.h>
-
-#include <iostream>
 
 float PI = 3.141592653589793238462643383;
 
@@ -84,17 +66,25 @@ std::string fToString(float f){
 
 }
 
+float vfilt(float sp, float vfilt_old, float t, float Tcon ){
+ return ((sp * t) + (vfilt_old * Tcon))/(t + Tcon);
+}
+
 int main(int argc, char * argv[]) try {
     
         if(argc < 2){
         std::cout << "Skriv argument för programmet, "
                   << "1/0 för hjul odo, 1/0 för predict-pose"
                   << std::endl;
+                  return 0;
         }
         
-       // cArduino arduino(ArduinoBaundRate::B9600bps);
+        serial s;
         
-        serial s("/dev/ttyACM0");
+        if(strcmp(argv[1],"1") == 0){
+        s.init("/dev/ttyACM0");
+        
+        }
         rs2::pipeline pipe;
         rs2::config cfg;
         rotation_estimator algo;
@@ -103,19 +93,31 @@ int main(int argc, char * argv[]) try {
         cfg.enable_stream(RS2_STREAM_ACCEL, RS2_FORMAT_MOTION_XYZ32F);
         cfg.enable_stream(RS2_STREAM_GYRO, RS2_FORMAT_MOTION_XYZ32F);  
       
-        
         auto pf = pipe.start(cfg);
           
         rs2::device dev = pf.get_device();
-        auto wheel_odom_snr = dev.first<rs2::wheel_odometer>();
+
+
+       auto wheel_odom_snr = dev.first<rs2::wheel_odometer>();
        
+       std::ifstream calibrationFile("realsenset265/forward_calib.json");
+       //std::cout << calibrationFile.bad() << " " << calibrationFile.fail() << std::endl; 
        
-        //std::ifstream calibrationFile("../forward_calib.json"); 
-        std::ifstream calibrationFile("T265/forward_calib.json");
-       // std::ifstream calibrationFile("T265/calibration_odometry.json");
-        //std::ifstream calibrationFile("T265/rightward_calib.json");
-      // std::ifstream calibrationFile("../downward_calib.json");
-      //  std::ifstream calibrationFile("realsenset265/calibration_odometry.json");
+       if(calibrationFile.fail()){
+        calibrationFile.close();
+        calibrationFile.open("T265/forward_calib.json");
+        }
+       
+       if(calibrationFile.fail()){
+        calibrationFile.close();
+        calibrationFile.open("../forward_calib.json");
+        } else {
+          std::cout << "kunde inte hitta json filen" << std::endl;
+          return 0;
+        }
+        
+       
+        
         const std::string json_str((std::istreambuf_iterator<char>(calibrationFile)),
                           std::istreambuf_iterator<char>());
                   
@@ -140,6 +142,11 @@ int main(int argc, char * argv[]) try {
         int count1 = 0;
         int count2 = 0;
         
+        float Tcon = 1;
+        float vfilt_old1 = 0;
+        float vfilt_old2 = 0;
+        
+        auto old_start = std::chrono::system_clock::now();
         while (true) {
             
             //testa att göra if satser på alla dessa för att kontrollera vilken frame det igentligen är!
@@ -151,6 +158,7 @@ int main(int argc, char * argv[]) try {
           
             auto fp = f.as<rs2::pose_frame>();
             
+
             auto pose_data = fp.get_pose_data();
             
                 
@@ -166,66 +174,69 @@ int main(int argc, char * argv[]) try {
               
             rs2_vector accel_data = accel_f.get_motion_data();
             algo.process_accel(accel_data);
-           
+          if(strcmp(argv[1],"1") == 0){
 	        do{
 	            temp = s.sread();
 	        } while(temp.size()< 9);
-	       
+	       }
 		 
+		   //  double thetaGrejFromCos = acos(pose_data.velocity.x / (abs(pose_data.velocity.x)+abs(pose_data.velocity.z)));
+		   //  double thetaGrejFromSin = asin(pose_data.velocity.z / (abs(pose_data.velocity.x)+abs(pose_data.velocity.z)));
+
+
+
+        //  auto quat = Eigen::Quaternion<double>(pose_data.rotation.w,pose_data.rotation.x,pose_data.rotation.y,pose_data.rotation.z);
+        //    auto rot = quat.toRotationMatrix();
+        //    auto euler = quat.toRotationMatrix().eulerAngles(0, 1, 2);
+           
+
 		     double theta = (PI/2)+algo.get_theta().y;
-		     
-		     //if(theta > )
-		     
-	        //arduino.flush();
+		    
+		   // std::cout << -euler(1)*(180/PI) << std::endl;
+		    
+		   // std::cout << (theta - (PI/2)-PI)*(180/PI) << std::endl; 
+		    
 	      	//std::this_thread::sleep_for(std::chrono::milliseconds(100));	
-	        //std::cout << temp << std::endl;
+	        std::cout << temp << std::endl;
 	        //std::cout << temp.size() << std::endl;
 	        if(temp.size() > 9){
             //std::cout << "Hastighet " << temp << std::endl;
 		        try{
-		        
-		            //speed1 = 10.5;
-		            //speed2 = 10.5;
+		            
 		            speed1 = std::stof (temp,&sz);
 		            speed2 = std::stof (temp.substr(sz),&sz1);
 		            
 		            count1 = std::stof (temp.substr(sz+sz1),&sz2);
 		            count2 = std::stof (temp.substr(sz+sz1+sz2));
 		            
-		          //  std::cout << "Hastighet " << speed1 << " " << speed2 << std::endl;
+		            auto start = std::chrono::system_clock::now();
+		            std::chrono::duration<float> elapSec = start-old_start;
+		            auto old_start = start;
 		            
+		            speed1 = vfilt(speed1, vfilt_old1, elapSec.count(), Tcon);
+		            vfilt_old1 = speed1;
+		            
+		            speed2 = vfilt(speed2, vfilt_old2, elapSec.count(), Tcon);
+		            vfilt_old2 = speed2;
+		            
+              //  std::cout << speed1 << " " << speed2 << std::endl;
+                
 		            //FORWARD
 		            sp1 = {cos(theta) * speed1, 0, sin(theta) * speed1 };
 		            sp2 = {cos(theta) * speed2, 0, sin(theta) * speed2 };
-		            
-		            //sp1 = {10.0, 10.0 ,10.0 };
-		            //sp2 = {10.0, 10.0 ,10.0 };
-		            
-		            
-		            //RIGHT
-		            //sp1 = {cos(algo.get_theta().y) * speed1, 0 ,sin(algo.get_theta().y) * speed1 };
-		            // sp2 = {cos(algo.get_theta().y) * speed2, 0 ,sin(algo.get_theta().y) * speed2 };
-
-                 
-		            
-		            //DOWN
-		            //sp1 = {sin(algo.get_theta().y) * speed1, cos(algo.get_theta().y) * speed1 ,0 };
-		            //sp2 = {sin(algo.get_theta().y) * speed2, cos(algo.get_theta().y) * speed1 ,0 };
-		            
+		              
 		            sp1_o = sp1;
 		            sp2_o = sp2;
 		            
 		        } catch(const std::exception& e){
-			        //std::cout <<"exception:("<< e.what() <<std::endl;
-			        //ifall kraschar pga illegal rs2_vector värde eller core dump avkommentera undre
 			        sp1 = sp1_o;
 			        sp2 = sp1_o;
-		            }
+		          }
 		    }
 		    
             if(strcmp(argv[1],"1") == 0){
-                bool b1 = wheel_odom_snr.send_wheel_odometry(0, f.get_frame_number(), sp1);
-                bool b2 = wheel_odom_snr.send_wheel_odometry(1, f.get_frame_number(), sp2);
+                bool b1 = wheel_odom_snr.send_wheel_odometry(0, fp.get_frame_number(), sp1);
+                bool b2 = wheel_odom_snr.send_wheel_odometry(1, fp.get_frame_number(), sp2);
             }
             
             if(strcmp(argv[2],"1") == 0){
@@ -238,18 +249,28 @@ int main(int argc, char * argv[]) try {
 		    
 		    auto ms = std::chrono::duration_cast< std::chrono::milliseconds >(std::chrono::system_clock::now().time_since_epoch());
 
+            //std::this_thread::sleep_for(std::chrono::milliseconds(200));
+            
+          //  std::cout << std::setprecision(3) << std::fixed << theta*(180/PI) << "   cos,x: " << cos(theta) << " " << pose_data.velocity.x 
+          //                                                                    << "   sin,z: " << sin(theta) << " " << pose_data.velocity.z << std::endl;
 
-            //std::cout << std::setprecision(3) << std::fixed << theta << " cos,x: " << cos(theta) << " sin,z: " << sin(theta) << " T265 hast: " << pose_data.velocity << std::endl; 
+        //    std::cout << std::setprecision(3) << std::fixed << euler(1)*(180/PI) << "   cos,x: " << sin(euler(1)) << " " << pose_data.velocity.x 
+        //                                                                      << "   sin,z: " << cos(euler(1)) << " " << pose_data.velocity.z << std::endl;
 
+            
+           // std::cout << std::setprecision(3) << std::fixed << theta*(180/PI) << "   cos,x: " << cos(theta) << " " << pose_data.velocity.x 
+           //                                                                   << "   sin,z: " << sin(theta) << " " << pose_data.velocity.z << std::endl;
+ 
+           
             std::cout   << std::setprecision(3) << std::fixed << 
                     "tid: " << ms.count() <<
                     " Pose: "  << pose_data.translation.x << " " << pose_data.translation.y << " " << pose_data.translation.z << 
-                    " Vinkel: " << theta << 
+                    " Vinkel: " << theta*(180/PI) << 
                     " confidence " << pose_data.tracker_confidence <<
                     "     Sp1: " << sp1 <<
                     "  T265 sp: " << pose_data.velocity << " " << count1 << " " << count2 << 
                     "     Sp2: " << sp2  << std::endl;
-        
+                    
             }
         
         return EXIT_SUCCESS;
